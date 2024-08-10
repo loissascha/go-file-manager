0570b6c27f54e4cd8070d49a79d1d1ea7c7adf4c
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-08-10 10:47:53 +0200

    sidebar show user folders

--- frontend/src/SideBar.tsx
+++ frontend/src/SideBar.tsx
@@ -1,18 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { GetFileList } from "../wailsjs/go/main/App";
 
 
 function SideBar() {
+    const [fixedFolders, setFixedFolders] = useState<string[]>([]);
 
     useEffect(() => {
+        setFixedFolders(["Home"]);
         GetFileList().then((files) => {
             console.log(files);
+            for (const file of files) {
+                if (file === "Pictures" || file === "Documents" || file === "Downloads" || file === "Music" || file === "Videos") {
+                    setFixedFolders(prev => [...prev, file]);
+                }
+            }
         });
     }, [])
 
     return (
-        <aside className="bg-gray-700">
-            Sidebar
+        <aside className="bg-gray-700 p-3">
+            <ul>
+                {fixedFolders.map((folder, index) => {
+                    return <li key={index}>{folder}</li>
+                })}
+            </ul>
         </aside>
     );
 }
