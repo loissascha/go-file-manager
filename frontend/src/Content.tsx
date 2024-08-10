c2e67cbd21f2e68f7780b603bd65ceefb6032fa1
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-08-10 13:31:40 +0200

    font awesome icons for folders and files

--- frontend/src/Content.tsx
+++ frontend/src/Content.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import { GetFileList } from "../wailsjs/go/main/App";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFolder } from "@fortawesome/free-regular-svg-icons";
+import { faFile } from "@fortawesome/free-regular-svg-icons";
 
 interface ContentProps {
     location: string;
@@ -35,11 +38,15 @@ function Content({ location, tabId, navigateToSubFolderDir }: ContentProps) {
         <div className="">
             <ul>
                 {files.map((file, index) => {
-                    return <li key={index} className='py-1 px-3 border-b last:border-b-0 border-gray-800' onClick={() => {
+                    return <li key={index} className='py-1 px-3 border-b last:border-b-0 border-gray-800 cursor-pointer' onClick={() => {
                         if (file.IsDir) {
                             navigateToSubFolderDir(file.Name);
                         }
                     }}>
+                        {file.IsDir ?
+                            <FontAwesomeIcon icon={faFolder} className='mr-2' /> :
+                            <FontAwesomeIcon icon={faFile} className='mr-2' />
+                        }
                         {file.Name}
                     </li>
                 })}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import './style.css'
+import './fontawesome.js'
 import App from './App'
 
 const container = document.getElementById('root')
