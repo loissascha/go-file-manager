fdf0ec931fa3e76aab55b86162661e37ce146c6f
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-08-10 10:42:30 +0200

    get file list in frontned

--- frontend/src/SideBar.tsx
+++ frontend/src/SideBar.tsx
@@ -1,6 +1,15 @@
+import { useEffect } from "react";
+import { GetFileList } from "../wailsjs/go/main/App";
 
 
 function SideBar() {
+
+    useEffect(() => {
+        GetFileList().then((files) => {
+            console.log(files);
+        });
+    }, [])
+
     return (
         <aside className="bg-gray-700">
             Sidebar
