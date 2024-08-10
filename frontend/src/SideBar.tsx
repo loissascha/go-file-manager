78e1a55416de2a547212dd438fcb9eff9116f8d8
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-08-10 16:36:18 +0200

    icons for side bar

--- frontend/src/SideBar.tsx
+++ frontend/src/SideBar.tsx
@@ -1,3 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { faFile } from "@fortawesome/free-solid-svg-icons";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
+import { faMusic } from "@fortawesome/free-solid-svg-icons";
+import { faImage } from "@fortawesome/free-solid-svg-icons";
+import { faVideo } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
 import { GetHomeFolderFileList } from "../wailsjs/go/main/App";
 
@@ -21,15 +28,40 @@ function SideBar({ navigateToHomeFolderDir }: SideBarProps) {
     }, [])
 
     return (
-        <aside className="bg-mocha-mantle py-4">
+        <aside className="bg-mocha-mantle">
             <ul>
-                <li className="cursor-pointer px-3 py-2 border-t border-b border-mocha-surface0" onClick={() => {
+                <li className="cursor-pointer px-3 py-2 border-b border-mocha-surface0" onClick={() => {
                     navigateToHomeFolderDir("");
-                }}>Home</li>
+                }}>
+                    <FontAwesomeIcon icon={faHouse} className='mr-2' />
+                    Home
+                </li>
                 {fixedFolders.map((folder, index) => {
                     return <li key={index} className='cursor-pointer px-3 py-2 border-b border-mocha-surface0' onClick={() => {
                         navigateToHomeFolderDir(folder);
-                    }}>{folder}</li>
+                    }}>
+                        {folder === "Documents" ?
+                            <FontAwesomeIcon icon={faFile} className='mr-2' /> :
+                            null
+                        }
+                        {folder === "Downloads" ?
+                            <FontAwesomeIcon icon={faDownload} className='mr-2' /> :
+                            null
+                        }
+                        {folder === "Music" ?
+                            <FontAwesomeIcon icon={faMusic} className='mr-2' /> :
+                            null
+                        }
+                        {folder === "Pictures" ?
+                            <FontAwesomeIcon icon={faImage} className='mr-2' /> :
+                            null
+                        }
+                        {folder === "Videos" ?
+                            <FontAwesomeIcon icon={faVideo} className='mr-2' /> :
+                            null
+                        }
+                        {folder}
+                    </li>
                 })}
             </ul>
         </aside>
