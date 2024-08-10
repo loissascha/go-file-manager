a85a96a713b7f2b3d7db981d0038853e2eef3367
Sascha Loishandl <sascha.loishandl@gmail.com>
2024-08-10 13:06:20 +0200

    sidebar design

--- frontend/src/SideBar.tsx
+++ frontend/src/SideBar.tsx
@@ -21,13 +21,13 @@ function SideBar({ navigateToHomeFolderDir }: SideBarProps) {
     }, [])
 
     return (
-        <aside className="bg-gray-700 p-3">
+        <aside className="bg-gray-700 py-4">
             <ul>
-                <li onClick={() => {
+                <li className="cursor-pointer px-3 py-2 border-t border-b border-gray-600" onClick={() => {
                     navigateToHomeFolderDir("");
                 }}>Home</li>
                 {fixedFolders.map((folder, index) => {
-                    return <li key={index} onClick={() => {
+                    return <li key={index} className='cursor-pointer px-3 py-2 border-b border-gray-600' onClick={() => {
                         navigateToHomeFolderDir(folder);
                     }}>{folder}</li>
                 })}
